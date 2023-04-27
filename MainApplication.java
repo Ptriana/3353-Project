ed2b452d78298b2c0833762c5c476fdace7583b0
Ptriana <112222539+Ptriana@users.noreply.github.com>
2023-04-26 20:02:44 -0400

    Add files via upload

---
+++ MainApplication.java
@@ -0,0 +1,28 @@
+package CalculatorGui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+public class MainApplication extends Application
+{
+   @Override
+   public void start(Stage stage) 
+   {
+       try
+       {
+       
+          Parent root = FXMLLoader.load(getClass().getResource("CalculatorGUIFXML.fxml"));
+          Scene scene = new Scene(root);
+          stage.setScene(scene);
+          stage.setTitle("Java Alchemist's Calculator App");
+          stage.show();
+       } catch(Exception e) 
+       {
+        e.printStackTrace();
+       }
+  }
+}
