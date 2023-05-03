71e683e001fe910fb4f1e312f9d3010f12012528
Ptriana <112222539+Ptriana@users.noreply.github.com>
2023-05-03 01:05:40 -0400

    Update SQLiteJDBC.java

--- CalculatorGui/SQLiteJDBC.java
+++ CalculatorGui/SQLiteJDBC.java
@@ -1,10 +1,13 @@
 import java.sql.*;
 
-public class SQLiteJDBC {
-  public static void main( String args[] ) {
+public class SQLiteJDBC 
+{
+  public static void main( String args[] ) 
+  {
       Connection c = null;
       
-      try {
+      try 
+      {
          Class.forName("org.sqlite.JDBC");
          c = DriverManager.getConnection("jdbc:sqlite:java.db");
       } catch ( Exception e ) {
